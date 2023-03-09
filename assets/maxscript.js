91d8b7416e3e069714645bc84fe87efb4d8f861d
Max Sokolich <50302377+MaxSokolich@users.noreply.github.com>
2023-03-09 17:50:18 -0500

    Update maxscript.js

--- assets/maxscript.js
+++ assets/maxscript.js
@@ -1,17 +1,6 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 int max = 3;
 int greg = 3;
 int maxgreg = max+greg;
+console.log(maxgreg)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
